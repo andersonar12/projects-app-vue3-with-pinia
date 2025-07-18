a8d3eecd1f8205215fe00dfebc202d3ad4d6633c
Anderson Romero <62034538+andersonar12@users.noreply.github.com>
2025-07-17 22:24:32 -0400

    feat: reorganizar importaciones y mejorar el componente FabButton

---
+++ src/modules/projects/components/FabButton.vue
@@ -0,0 +1,35 @@
+<template>
+  <button
+    :class="['btn btn-circle btn-secondary p-1 fixed', position]"
+    @click="$emit('click')"
+  >
+    <slot />
+  </button>
+</template>
+<script setup lang="ts">
+interface Props {
+  position?: "top-left" | "top-right" | "bottom-left" | "bottom-right";
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  position: "bottom-right",
+});
+
+defineEmits(["click"]);
+</script>
+<style scoped>
+@reference "tailwindcss";
+
+.top-left {
+  @apply top-9 left-9;
+}
+.top-right {
+  @apply top-9 right-9;
+}
+.bottom-left {
+  @apply bottom-9 left-9;
+}
+.bottom-right {
+  @apply bottom-9 right-9;
+}
+</style>
