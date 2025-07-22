6ffa9b5987d73088b2656046bee015d83e7e06ef
Anderson Romero <62034538+andersonar12@users.noreply.github.com>
2025-07-21 20:13:59 -0400

    feat: agregar modales personalizados y reorganizar botones en la vista de proyectos

---
+++ src/modules/common/components/InputModal.vue
@@ -0,0 +1,60 @@
+<template>
+  <dialog class="modal" :open="open">
+    <div class="modal-box">
+      <h3 class="text-lg font-bold">{{ title }}</h3>
+      <p class="py-4">{{ subtitle }}</p>
+      <div class="modal-action flex flex-col">
+        <form method="dialog" @submit.prevent="submitValue">
+          <input
+            ref="inputRef"
+            type="text"
+            placeholder="Nombre de proyecto"
+            class="input input-bordered input-primary w-full flex-1"
+            v-model="inputValue"
+          />
+          <!-- if there is a button in form, it will close the modal -->
+          <div class="flex justify-end mt-5">
+            <button type="button" class="btn mr-4" @click="$emit('close')">
+              Cerrar
+            </button>
+            <button class="btn btn-primary">Acpetar</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </dialog>
+  <div
+    v-if="open"
+    class="modal-backdrop fixed top-0 left-0 z-10 bg-black opacity-40 w-screen h-screen"
+  ></div>
+</template>
+<script setup lang="ts">
+import { ref } from "vue";
+
+interface Props {
+  open: boolean;
+  title?: string;
+  subtitle?: string;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  open: false,
+});
+
+const emit = defineEmits<{ close: [void]; value: [text: string] }>();
+
+const inputValue = ref("");
+const inputRef = ref<HTMLInputElement | null>(null);
+
+const submitValue = () => {
+  if (!inputValue.value) {
+    inputRef.value?.focus();
+    return;
+  }
+  emit("value", inputValue.value.trim());
+  emit("close");
+
+  inputValue.value = "";
+};
+</script>
+<style></style>
--- src/modules/projects/views/ProjectsView.vue
+++ src/modules/projects/views/ProjectsView.vue
@@ -20,14 +20,56 @@
         </tr>
       </tbody>
     </table>
+    <InputModal
+      :open="modalOpen"
+      @close="modalOpen = false"
+      @value="onNewValue($event)"
+      title="'Añadir Proyecto'"
+      subtitle="'Ingrese los datos para añadir nuevo proyecto'"
+    />
 
-    <FabButton @click="console.log('click')">
+    <CustomModal :open="customModalOpen" @close="customModalOpen = false">
+      <template #header>
+        <h1 class="text-2xl">Titulo del modal</h1>
+      </template>
+
+      <template #body>
+        <p>Contenido del modal</p>
+      </template>
+
+      <template #actions>
+        <div class="flex justify-end mt-2">
+          <button class="btn mr-4" @click="customModalOpen = false">
+            Cerrar
+          </button>
+          <button class="btn btn-primary" @click="customModalOpen = false">
+            Acpetar
+          </button>
+        </div>
+      </template>
+    </CustomModal>
+
+    <FabButton @click="modalOpen = true">
       <AddCircle />
+    </FabButton>
+    <FabButton @click="customModalOpen = true" class="bottom-left">
+      <ModalIcon />
     </FabButton>
   </div>
 </template>
 <script setup lang="ts">
 import AddCircle from "@/modules/common/icons/AddCircle.vue";
-import FabButton from "../components/FabButton.vue";
+import FabButton from "../../common/components/FabButton.vue";
+import InputModal from "@/modules/common/components/InputModal.vue";
+import { ref } from "vue";
+import CustomModal from "@/modules/common/components/CustomModal.vue";
+import ModalIcon from "@/modules/common/icons/ModalIcon.vue";
+
+const modalOpen = ref(false);
+const customModalOpen = ref(false);
+
+const onNewValue = (projectName: string) => {
+  console.log({ projectName });
+};
 </script>
 <style lang=""></style>
